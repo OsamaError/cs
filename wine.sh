65b4e0d2ebc6360fb165c250bd0589cff454d443
OsamaError <91324275+OsamaError@users.noreply.github.com>
2022-06-21 10:29:53 +0200

    Update wine.sh

--- wine.sh
+++ wine.sh
@@ -1,8 +1,10 @@
-sudo apt-get install wine64 -y
+sudo apt install wine64 -y
 sudo dpkg --add-architecture i386
 wget -qO- https://dl.winehq.org/wine-builds/Release.key | sudo apt-key add -
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv F987672F
-sudo apt-get install wine-stable -y
-sudo apt-get install wine-development -y
+sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
+apt-get update
+sudo apt-get install --install-recommends winehq-stable
+sudo apt-get install --install-recommends winehq-devel
 clear
-Wine --version
+wine --version
