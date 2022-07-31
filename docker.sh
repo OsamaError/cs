0bd6386100d1565e81df6663de9a54d848ac7dda
OsamaError <91324275+OsamaError@users.noreply.github.com>
2022-07-31 12:25:10 +0200

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,3 @@
+sudo apt install novnc -y
+clear
+docker run -it --network host --shm-size 4g --privileged --cap-add=NET_ADMIN thuonghai2711/ubuntu22-vnc-pulseaudio:xfce4 && /usr/share/novnc/utils/launch.sh --listen 8081 --vnc localhost:5900
