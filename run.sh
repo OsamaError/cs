d35d27413fb5bbb339bb7a0d12daec52ba9f2d52
OsamaError <91324275+OsamaError@users.noreply.github.com>
2023-10-03 18:56:24 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 sudo apt update
 DEBIAN_FRONTEND=noninteractive sudo apt upgrade -y
-DEBIAN_FRONTEND=noninteractive sudo apt install --assume-yes wget tasksel
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb
-DEBIAN_FRONTEND=noninteractive sudo apt install --assume-yes xfce4 xfce4-goodies desktop-base dbus-x11 xscreensaver
+DEBIAN_FRONTEND=noninteractive sudo apt install --assume-yes tasksel xfce4 xfce4-goodies desktop-base dbus-x11 xscreensaver firefox
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
 clear
 echo "https://remotedesktop.google.com/headless"
